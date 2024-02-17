b558d259ac063e6f453fddb21ac4b063fdc161a7
Brandon Vu <bvu32@gatech.edu>
2024-02-17 01:45:05 -0500

    fix MEDIA_PATH import

--- clipplex/models/snapshot.py
+++ clipplex/models/snapshot.py
@@ -1,4 +1,4 @@
-from clipplex.config import MEDIA_PATH
+from clipplex.config import MEDIA_DIR_PATH
 import subprocess
 
 
@@ -9,7 +9,7 @@ class Snapshot:
         self.fps = int(fps)
 
     def _download_frames(self):
-        cmd = f"ffmpeg -ss {self.time} -i {self.media_path} -vframes {self.fps} -qscale:v 2 {MEDIA_PATH}/images/{self.time.replace(':','_')}_%03d.jpg"
+        cmd = f"ffmpeg -ss {self.time} -i {self.media_path} -vframes {self.fps} -qscale:v 2 {MEDIA_DIR_PATH}/images/{self.time.replace(':','_')}_%03d.jpg"
         a = subprocess.call(
             cmd, shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL
         )
