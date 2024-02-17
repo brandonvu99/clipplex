e41d45f8b40af7aec5b0424fa81d0d006e42637b
Brandon Vu <bvu32@gatech.edu>
2024-02-17 01:43:10 -0500

    configure logging

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from clipplex import flaskapp
+import logging
+
+def main():
+    logging.basicConfig(
+        format="%(asctime)s [%(levelname)s] %(filename)s: %(message)s",
+        level=logging.NOTSET,
+        handlers=[
+            logging.FileHandler(filename="./logs/clipplex.log", encoding="utf-16"),
+            logging.StreamHandler(),
+        ],
+    )
+
+if __name__ == '__main__':
+    main()
