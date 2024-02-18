8558106482ea6ef415dfd7348fbd5044eebab7f6
Brandon Vu <bvu32@gatech.edu>
2024-02-17 19:31:18 -0500

    move plex_filepath_to_clipplex_filepath into PlexInfo class

--- clipplex/models/plex.py
+++ clipplex/models/plex.py
@@ -1,5 +1,5 @@
 from clipplex.utils.timing import milli_to_string
-from clipplex.config import PLEX_TOKEN, PLEX_URL
+from clipplex.config import PLEX_TOKEN, PLEX_URL, PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH
 from clipplex.utils.files import plex_filepath_to_clipplex_filepath
 from pathlib import Path
 from xml.etree import ElementTree
@@ -85,7 +85,7 @@ class PlexInfo:
             0
         ].attrib  # REPLACE THAT BY A FIND PART TAG
         plex_filepath = Path(media_dict["file"])
-        clipplex_filepath = plex_filepath_to_clipplex_filepath(plex_filepath)
+        clipplex_filepath = PlexInfo.plex_filepath_to_clipplex_filepath(plex_filepath)
         return clipplex_filepath
 
     def _get_file_title(self) -> str:
@@ -145,3 +145,34 @@ class PlexInfo:
                 if session.tag == "User" and session.attrib["title"] == username:
                     return list(self.sessions_xml).index(sessions)
         raise Exception(f"No stream running for user {username}")
+
+    @staticmethod
+    def plex_filepath_to_clipplex_filepath(plex_filepath: Path) -> Path:
+        """
+        Converts a given plex filepath to its respective clipplex filepath based off of the mapping {PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH}.
+        The most specific (or lowest common ancestor) of all {PLEX_DIRPATH}s for the given plex filepath is chosen. For example, for the following mapping:
+
+        PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH = {
+            "/long/plex/path/to/media/" : "/clipplex/Media/",
+            "/long/plex/path/to/media/but/more/specific/" : "/clipplex/other/media/dir/",
+        }
+
+        a plex filepath of "/long/plex/path/to/media/but/more/specific/Drag Race/s01e01.mkv" will be mapped to "/clipplex/other/media/dir/Drag Race/s01e01.mkv"
+        because even though "/long/plex/path/to/media/" COULD match, the more specific one matches.
+        """
+
+        plex_dirpath_ancestors = [
+            plex_dirpath
+            for plex_dirpath in PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH.keys()
+            if plex_filepath.is_relative_to(plex_dirpath)
+        ]
+        if not plex_dirpath_ancestors:
+            raise ValueError(
+                f"The given plex_filepath ({plex_filepath}) does not have an associated mapping in PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH."
+            )
+        most_specific_plex_dir = Path(os.path.commonpath(plex_dirpath_ancestors))
+
+        clipplex_dirpath = PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH[most_specific_plex_dir]
+        relative_filepath = plex_filepath.relative_to(most_specific_plex_dir)
+
+        return clipplex_dirpath / relative_filepath
--- clipplex/utils/files.py
+++ clipplex/utils/files.py
@@ -10,42 +10,9 @@ import ffmpeg
 import os
 
 
-
 def delete_file(self, file_path) -> bool:
     try:
         os.remove(f"./app/{file_path}")
         return True
     except:
         return False
-
-
-def plex_filepath_to_clipplex_filepath(plex_filepath: Path) -> Path:
-    """
-    Converts a given plex filepath to its respective clipplex filepath based off of the mapping {PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH}.
-    The most specific (or lowest common ancestor) of all {PLEX_DIRPATH}s for the given plex filepath is chosen. For example, for the following mapping:
-
-    PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH = {
-        "/long/plex/path/to/media/" : "/clipplex/Media/",
-        "/long/plex/path/to/media/but/more/specific/" : "/clipplex/other/media/dir/",
-    }
-
-    a plex filepath of "/long/plex/path/to/media/but/more/specific/Drag Race/s01e01.mkv" will be mapped to "/clipplex/other/media/dir/Drag Race/s01e01.mkv"
-    because even though "/long/plex/path/to/media/" COULD match, the more specific one matches.
-    """
-
-    plex_dirpath_ancestors = [
-        plex_dirpath
-        for plex_dirpath in PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH.keys()
-        if plex_filepath.is_relative_to(plex_dirpath)
-    ]
-    if not plex_dirpath_ancestors:
-        raise ValueError(
-            f"The given plex_filepath ({plex_filepath}) does not have an associated mapping in PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH."
-        )
-    most_specific_plex_dir = Path(os.path.commonpath(plex_dirpath_ancestors))
-
-    clipplex_dirpath = PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH[most_specific_plex_dir]
-    relative_filepath = plex_filepath.relative_to(most_specific_plex_dir)
-
-    return clipplex_dirpath / relative_filepath
-
