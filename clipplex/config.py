faaf21c94f88e58e8a241c4c25d324fb15618335
Brandon Vu <bvu32@gatech.edu>
2024-02-19 04:00:37 -0500

    read plex to clipplex dirpaths from config file

--- clipplex/config.py
+++ clipplex/config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import yaml
 
 GENERATED_MEDIA_DIRPATH: Path = Path("./clipplex/static/generated_media")
 IMAGES_DIRPATH: Path = GENERATED_MEDIA_DIRPATH / "images"
@@ -7,8 +8,13 @@ CLIPS_DIRPATH: Path = GENERATED_MEDIA_DIRPATH / "clips"
 IMAGES_DIRPATH.mkdir(parents=True, exist_ok=True)
 CLIPS_DIRPATH.mkdir(parents=True, exist_ok=True)
 
+with open("config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH: dict[Path, Path] = {
-    Path(r"E:/Media/") : Path(r"../Media")
+    Path(plex_dirpath): Path(clipplex_dirpath)
+    for plex_dirpath, clipplex_dirpath in config[
+        "PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH"
+    ].items()
 }
 
 PLEX_TOKEN = os.environ.get("PLEX_TOKEN")
