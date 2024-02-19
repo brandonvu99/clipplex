b0ab742d741ae4dcf214c79a033a699ebf205805
Brandon Vu <bvu32@gatech.edu>
2024-02-19 04:21:38 -0500

    create default config if it does not exist

--- clipplex/config.py
+++ clipplex/config.py
@@ -8,7 +8,16 @@ CLIPS_DIRPATH: Path = GENERATED_MEDIA_DIRPATH / "clips"
 IMAGES_DIRPATH.mkdir(parents=True, exist_ok=True)
 CLIPS_DIRPATH.mkdir(parents=True, exist_ok=True)
 
-with open("config/config.yaml", "r") as f:
+CONFIG_DEFAULT: dict = {
+    "PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH": {}
+}
+CONFIG_DIRPATH: Path = Path("./config/")
+CONFIG_DIRPATH.mkdir(parents=True, exist_ok=True)
+CONFIG_FILEPATH: Path = CONFIG_DIRPATH / "config.yaml"
+if not CONFIG_FILEPATH.exists():
+    with open(CONFIG_FILEPATH, "w") as f:
+        yaml.dump(CONFIG_DEFAULT, f)
+with open(CONFIG_FILEPATH, "r") as f:
     config = yaml.safe_load(f)
 PLEX_DIRPATH_TO_CLIPPLEX_DIRPATH: dict[Path, Path] = {
     Path(plex_dirpath): Path(clipplex_dirpath)
