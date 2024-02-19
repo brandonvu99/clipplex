9716eee6e1815a90f783a22c5952a784236c721f
Brandon Vu <bvu32@gatech.edu>
2024-02-19 00:24:11 -0500

    create logs directory on startup

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import logging
+from pathlib import Path
+LOG_DIRPATH = Path("../logs")
+LOG_DIRPATH.mkdir(parents=True, exist_ok=True)
+
 logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(filename)s: %(message)s",
     level=logging.NOTSET,
     handlers=[
-        logging.FileHandler(filename="../logs/clipplex.log", encoding="utf-16"),
+        logging.FileHandler(filename=LOG_DIRPATH / "clipplex.log", encoding="utf-16"),
         logging.StreamHandler(),
     ],
 )
